9f4bfc85a3575cf3b083ac775dd69adf524e9bef
chuongdks <phchuogn1998@gmail.com>
2024-05-18 10:54:35 -0400

    Added Assignment 1 for COMP-3340

---
+++ domains/pham75.myweb.cs.uwindsor.ca/public_html/a1.php
@@ -0,0 +1,45 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First COMP3340 Web Page</title>
+    <link rel="stylesheet" href="a1.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to my first Web Page</h1>
+        <h2>Have a look around</h2>
+        <div class="header-images">
+            <a href="https://www.youtube.com/watch?v=9K2Y-rfUy_4" target="_blank" rel="noopener noreferrer"><img src="./images/cat1.jpg"></a>
+            <img src="./images/cat2.jpg">
+        </div>
+    </header>
+
+    <body>
+        <p>This is my first Web Page for Assignment #1 of COMP-3340. I can put random stuff on the web now. <br>
+        Sorry about the background color and text color. I have no saying in this.</p>
+        <p>I can put any link on this line. Like <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">this</a></p>
+
+        <p>Here is a table of really good self-defense technique!</p>
+        <table>
+            <tr>
+                <th>Move name</th>
+                <th>Video on how to do it</th>
+            </tr>
+            <tr>
+                <td>Yeltsa Kcir</td>
+                <td><a href="https://www.youtube.com/shorts/xHEgHjJvR94" target="_blank" rel="noopener noreferrer">Link</a></td>
+            </tr>
+            <tr>
+                <td>Pistol Defenses </td>
+                <td><a href="https://www.youtube.com/watch?v=5uO0wsEUBN0" target="_blank" rel="noopener noreferrer">Link</a></td>
+            </tr>
+        </table> 
+    </body>
+
+    <footer>
+        <p>Author: Chuong Pham<br>
+        Contact: <a href="mailto:pham75@uwindsor.ca">pham75@uwindsor.ca</a></p>
+    </footer>
+</body>
+</html>
