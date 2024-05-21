124a93beaba0bda72589eae7f4f3d883f43c7d1d
chuongdks <phchuogn1998@gmail.com>
2024-05-21 18:36:31 -0400

    Added stuff for the subdomain comp3340.pham75

--- domains/comp3340.pham75.myweb.cs.uwindsor.ca/public_html/a1.php
+++ domains/comp3340.pham75.myweb.cs.uwindsor.ca/public_html/a1.php
@@ -10,7 +10,7 @@
         <h1>Welcome to my first Web Page</h1>
         <h2>Have a look around</h2>
         <div class="header-images">
-            <a href="https://www.youtube.com/watch?v=9K2Y-rfUy_4"><img src="./images/cat1.jpg"></a>
+            <a href="https://www.youtube.com/watch?v=9K2Y-rfUy_4" target="_blank" rel="noopener noreferrer"><img src="./images/cat1.jpg"></a>
             <img src="./images/cat2.jpg">
         </div>
     </header>
@@ -18,7 +18,7 @@
     <body>
         <p>This is my first Web Page for Assignment #1 of COMP-3340. I can put random stuff on the web now. <br>
         Sorry about the background color and text color. I have no saying in this.</p>
-        <p>I can put any link on this line. Like <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">this</a></p>
+        <p>I can put any link on this line. Like <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">this</a></p>
 
         <p>Here is a table of really good self-defense technique!</p>
         <table>
@@ -28,11 +28,11 @@
             </tr>
             <tr>
                 <td>Yeltsa Kcir</td>
-                <td><a href="https://www.youtube.com/shorts/xHEgHjJvR94">Link</a></td>
+                <td><a href="https://www.youtube.com/shorts/xHEgHjJvR94" target="_blank" rel="noopener noreferrer">Link</a></td>
             </tr>
             <tr>
                 <td>Pistol Defenses </td>
-                <td><a href="https://www.youtube.com/watch?v=5uO0wsEUBN0">Link</a></td>
+                <td><a href="https://www.youtube.com/watch?v=5uO0wsEUBN0" target="_blank" rel="noopener noreferrer">Link</a></td>
             </tr>
         </table> 
     </body>
