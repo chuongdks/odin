9d8de48f49442379b9c41cdb337ac429c4dca37c
chuongdks <phchuogn1998@gmail.com>
2024-06-07 22:34:16 -0400

    Added stuff: + Randomize color + Polish most of the function

--- project/2_EASk/script.js
+++ project/2_EASk/script.js
@@ -1,5 +1,57 @@
+let mode = 0;
+
+// Random color RGB style, use in DrawSquare() function in backgroundColor
+const randomRGBColor = `rgb(${Math.floor(Math.random() * 255)},
+                            ${Math.floor(Math.random() * 255)},    
+                            ${Math.floor(Math.random() * 255)})`;
+
+// Random color HEX style, use in DrawSquare() function in backgroundColor
+const randomHexColor = () => {
+    let n = (Math.floor(Math.random() * 0xffffff).toString(16));
+    return `#${n.padStart(6,'0')}`;
+};
+
+function DrawSquare(e) 
+{
+    function isWhite(color) 
+    {
+        return color === "white" || color === "rgb(255, 255, 255)" || color === "#ffffff";
+    }   
+
+    // console.log(e.target.style.backgroundColor);
+
+    if (e.target.classList.contains('square') && isDrawing && isWhite(e.target.style.backgroundColor)) 
+    {
+        e.target.style.backgroundColor = randomHexColor(); 
+    }
+}
+
 let maxSquare = 50;
 const container = document.querySelector("#container");
+
+function createSquare ()
+{
+    // Checking for 'firstChild' is usually faster than checking the 'lastChild'
+    while (container.firstChild) 
+    {
+        container.removeChild(container.lastChild);
+    }      
+    
+    // Create the board function
+    for (let i = 0; i < maxSquare; i++)
+    {
+        const squareContainer = document.createElement("div");
+        squareContainer.classList.add("squareContainer");    
+        container.appendChild(squareContainer);
+        for (let j = 0; j < maxSquare; j++)
+        {
+            const square = document.createElement("div");
+            square.style.backgroundColor = "white";
+            square.classList.add("square");   
+            squareContainer.appendChild(square);
+        }
+    }      
+}
 
 // Create the square div by using 2 container instead of 1 container
 for (let i = 0; i < maxSquare; i++)
@@ -10,17 +62,26 @@ for (let i = 0; i < maxSquare; i++)
     for (let j = 0; j < maxSquare; j++)
     {
         const square = document.createElement("div");
+        square.style.backgroundColor = "white";
         square.classList.add("square");   
         squareContainer.appendChild(square);
     }
 }
 
-// Add a single event listener to the container
-container.addEventListener('mouseover', (event) => 
-{
-    if (event.target.classList.contains('square')) 
+// Add the event listeners for mousedown, mousemove, and mouseup
+let isDrawing = false;
+
+container.addEventListener("mousedown", () => {
+    isDrawing = true;
+});
+
+container.addEventListener('mousemove',  DrawSquare);
+container.addEventListener('mousedown', DrawSquare);
+
+window.addEventListener("mouseup", () => {
+    if (isDrawing) 
     {
-        event.target.style.backgroundColor = "blue";
+        isDrawing = false;
     }
 });
 
@@ -28,6 +89,7 @@ container.addEventListener('mouseover', (event) =>
 const input = document.querySelector("input");
 const button  = document.querySelector("#changeSize");
 const buttonReset  = document.querySelector("#reset");
+input.value = "";
 
 // Function for changing board size
 button.addEventListener("click", () => 
@@ -46,7 +108,7 @@ button.addEventListener("click", () =>
         container.removeChild(container.lastChild);
     }    
 
-    // 
+    // Create the board function
     for (let i = 0; i < maxSquare; i++)
     {
         const squareContainer = document.createElement("div");
@@ -55,6 +117,7 @@ button.addEventListener("click", () =>
         for (let j = 0; j < maxSquare; j++)
         {
             const square = document.createElement("div");
+            square.style.backgroundColor = "white";
             square.classList.add("square");   
             squareContainer.appendChild(square);
         }
@@ -70,7 +133,7 @@ buttonReset.addEventListener("click", () =>
         container.removeChild(container.lastChild);
     }      
 
-    // 
+    // Create the board function
     for (let i = 0; i < maxSquare; i++)
     {
         const squareContainer = document.createElement("div");
@@ -79,6 +142,7 @@ buttonReset.addEventListener("click", () =>
         for (let j = 0; j < maxSquare; j++)
         {
             const square = document.createElement("div");
+            square.style.backgroundColor = "white";
             square.classList.add("square");   
             squareContainer.appendChild(square);
         }
