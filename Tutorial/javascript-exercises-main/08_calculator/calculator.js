29fe7ef078fe955566083b2326ea099cdf8ffbd1
chuongdks <phchuogn1998@gmail.com>
2024-06-23 00:28:09 -0400

    asd

--- Tutorial/javascript-exercises-main/08_calculator/calculator.js
+++ Tutorial/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let pow = 1;
+  for (let x = 0; x < exp; x++)
+  {
+    pow *= base;
+  }
+	return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	while (num != 0)
+  {
+    fact *= num;
+    num--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- Tutorial/javascript-exercises-main/09_palindromes/palindromes.js
+++ Tutorial/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // 
+    const validCharacter = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    // 
+    const splitCleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => validCharacter.includes(char))
+    .join("");
+
+    // 
+    const reversedString = splitCleanString.split("").reverse().join("");
+
+    // 
+    return splitCleanString === reversedString;
 };
 
 // Do not edit below this line
